f3a10f41f389127280b40b21ac6d4f9c2830c6a5
StepaniaH <StepaniaH0907@gmail.com>
2025-04-08 17:36:53 +0800

    feat(zsh): init zsh

---
+++ dot_config/zsh/aliases.zsh
@@ -0,0 +1,14 @@
+# All aliases of zsh
+
+alias ls='ls -G'                                       # list with colors
+alias ll='eza -lah'                                    # list with colors and long format
+alias c='clear'                                        # clear terminal
+alias h='history'                                      # show history
+alias g='lazygit'                                      # git gui Lazygit
+alias f='fastfetch'                                    # show neofetch
+alias bu='brew update && brew upgrade && brew cleanup' # update brew and upgrade, cleanup
+alias vim='nvim', vi='nvim', v='nvim'                  # neovim 
+alias cal='calcurse'                                   # calcurse
+alias news='newsboat'                                  # newsboat
+alias python='python3'                                 # python3
+alias pip='pip3'
