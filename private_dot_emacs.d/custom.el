a843b86f45f5e9ad8705cc6c84001634807c60bc
StepaniaH <StepaniaH0907@gmail.com>
2025-04-08 18:10:57 +0800

    feat(emacs): init emacs again

---
+++ private_dot_emacs.d/custom.el
@@ -0,0 +1,23 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mouse-wheel-progressive-speed nil)
+ '(package-selected-packages
+   '(ace-window amx catppuccin-theme company company-box counsel
+		counsel-projectile dashboard flycheck good-scroll
+		highlight-symbol hydra ivy lsp-ivy lsp-mode
+		lsp-treemacs lsp-ui marginalia multiple-cursors mwim
+		org-roam rainbow-delimiters smart-mode-line tiny
+		treemacs treemacs-projectile undo-tree use-package
+		use-package-hydra wakatime-mode yasnippet
+		yasnippet-snippets))
+ '(wakatime-cli-path "~/.wakatime/wakatime-cli"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
